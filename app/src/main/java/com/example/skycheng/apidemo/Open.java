6619c958a8b5b7a71d29e3a80a31eb2d59b2cc04
Sky Cheng <Skycheng@qq.com>
2017-09-26 09:38:34 +0800

    情况1:领取红包成功

--- app/src/main/java/com/example/skycheng/apidemo/Open.java
+++ app/src/main/java/com/example/skycheng/apidemo/Open.java
@@ -3,15 +3,39 @@ package com.example.skycheng.apidemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 /**
  * Created by SkyCheng on 2017/9/25.
  */
 
 public class Open extends AppCompatActivity{
+
+    private Button mClose;
+    private ImageButton mHeadImage;
+    private TextView mName;
+    private TextView mMoney;
+    private TextView mSearch;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.open);
+        initView();
+    }
+
+    private void initView() {
+        mClose = (Button) findViewById(R.id.close);
+        mHeadImage = (ImageButton) findViewById(R.id.head_img);
+        mName = (TextView) findViewById(R.id.name);
+        mMoney = (TextView) findViewById(R.id.money);
+        mSearch = (TextView) findViewById(R.id.search);
+    }
+
+    public void close(View view) {
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/skycheng/apidemo/OpenFail.java
@@ -0,0 +1,13 @@
+package com.example.skycheng.apidemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenFail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_fail);
+    }
+}
